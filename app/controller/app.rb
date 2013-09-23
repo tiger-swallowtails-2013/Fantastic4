83904cf95c459fd79a755fd57961032df90f413d
Jonathan Chew <jon.j.chew@gmail.com>
2013-09-23 16:37:09 -0700

    changed config.ru for heroku

--- app/controller/app.rb
+++ app/controller/app.rb
@@ -1,32 +1,36 @@
-require 'sinatra'
+  require 'sinatra'
+  class App < Sinatra::Base
 
-enable :sessions
 
-get '/' do
-  session[:text] = "It just comes back in flashes you know"
-  @text = session[:text]
-  @ready = session[:ready]
-  erb :index
-end
+    enable :sessions
 
-post '/ready' do
-  session[:ready] = true
-  session[:starttime] = Time.now
-  redirect '/'
-end
+    get '/' do
+      session[:text] = "It just comes back in flashes you know"
+      @text = session[:text]
+      @ready = session[:ready]
+      erb :index
+    end
 
-post '/results' do
-  if params[:user_input] == session[:text]
-    session[:endtime] = Time.now
-    session[:wpm] = 8*60/(session[:endtime]-session[:starttime]) 
-    redirect '/results'
+    post '/ready' do
+      session[:ready] = true
+      session[:starttime] = Time.now
+      redirect '/'
+    end
+
+    post '/results' do
+      if params[:user_input] == session[:text]
+        session[:endtime] = Time.now
+        session[:wpm] = 8*60/(session[:endtime]-session[:starttime]) 
+        redirect '/results'
   #else
   #   "didn't type correctly"
   #   erb :index
-  end
+end
 end
 
 get '/results' do
   @wpm = session[:wpm]
   erb :results
+end
+
 end
