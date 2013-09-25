8382df13c47552e8f8bf6e6cee25ffcd0ff0f385
Apprentice <apprentice@devbootcamp.com>
2013-09-25 14:59:08 -0700

    took out commented code

--- app/controller/public/test.js
+++ app/controller/public/test.js
@@ -22,8 +22,9 @@ var Game = function(){
     var game_started = false
     var userInputElement = document.getElementById('userArea');
     var gameText = document.getElementById('game_text');
+
     userInputElement.addEventListener('keyup', function() {
-      console.log("add event listener")
+      console.log("in event listener")
 
       //create a new timer, and start the timer
       if (game_started === false){
@@ -45,37 +46,26 @@ var Game = function(){
   }
 
 
-  //, move_turtle: function(){
+  // , move_turtle: function(){
   //   var image = document.getElementById('turtle');
   //   console.log(image);
   //   image.style.marginleft += "20px"
   // },
-
-  // match_text: function(string) {
-  //   if string === 
-
-  // }
 }
 
 
 var Matcher = function (input,match_text_element){
    console.log("running matcher")
    var output={}
-  // var match_text_element = document.getElementById('game_text');
-  // var input = document.getElementById('userArea');
-  // input.addEventListener('keyup', function(){
     var x = input.value.length;
     var match_area = match_text_element.innerText.substring(0,x);
     if(input.value === match_area){
       match_text_element.innerHTML = "<span style='color:green'><u>" + match_area + "</u></span>" + match_text_element.innerText.replace(match_area,"")
-      //Game.move_turtle()
+      Game.move_turtle()
     }
     if(input.value === match_text_element.innerText){
       console.log("complete")
       output.match = true
-      //stop the timer
-      //calculate wpm
-      //display wpm
     }
 
     return output
